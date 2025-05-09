9debff751c34778f49e500eb4eb71ba2f52d0c01
Dian Allan Acub <dianallanm.acub23@gmail.com>
2025-05-09 10:04:17 +0800

    HELp

--- profile/profile.js
+++ profile/profile.js
@@ -8,24 +8,5 @@ btnHome.addEventListener("click", function () {
 btnAbout.addEventListener("click", function () {
   window.location.href = "../about/about.html";
 });
-/*
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("/user/get_user.php")
-    .then((res) => res.json())
-    .then((userData) => {
-      if (userData && userData.username) {
-        document.querySelector(
-          ".username"
-        ).textContent = `@${userData.username}`;
-        // add more user data here in future, when database is done
-      } else {
-        document.querySelector(".username").textContent = "@Guest";
-      }
-    })
-    .catch((error) => {
-      console.error("Failed to load user data:", error);
-      document.querySelector(".username").textContent = "@Guest";
-    });
-});
-*/
+
 
